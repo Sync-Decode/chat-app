4e518e96f5fb1e7f832e1f2f643ed2db84045053
Sync-Decode <synkibe@gmail.com>
2025-06-23 14:55:16 +0300

    mvp redirect

--- .history/src/components/GoogleSignin_20250623144530.jsx
+++ .history/src/components/GoogleSignin_20250623144530.jsx
@@ -7,9 +7,7 @@ const GoogleSignin = ({ text }) => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: typeof window !== 'undefined'
-      ? window.location.origin + '/auth/callback'
-      : undefined,,
+        redirectTo: '/'
         scope: 'openid email profile',
       },
     })
